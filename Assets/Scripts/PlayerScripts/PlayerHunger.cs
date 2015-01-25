38fe8329907897f8b77a6ff02d436d2fbcb9b0f1
WatkinsEric <eric_watkins_6@hotmail.com>
2015-01-25 14:09:08 -0700

    created a hunger script

---
+++ Assets/Scripts/PlayerScripts/PlayerHunger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerHunger : MonoBehaviour {
+	public float startingHunger = 14400;
+	public float currentHunger;
+	public Image hungerImage;
+	public AudioClip deathClip;
+	bool isDead;
+	public Text hungerText;
+	int hunger;
+	// Use this for initialization
+	void Start () {
+		currentHunger = startingHunger;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentHunger=currentHunger-1;
+		hunger = (int)((currentHunger / startingHunger) * 100);
+		hungerText.text=hunger.ToString();
+	}
+}
